efe203f26a985931f63f6bfef345e2e7f3bb5543
DefenetlyNotSimon <simon21.blum@gmail.com>
2022-02-24 20:11:17 +0100

    basic first version

---
+++ index.js
@@ -0,0 +1,14 @@
+//Requirements
+const { Client, Intents} = require('discord.js');
+const { token } = require('config.js');
+
+//Neue Client Instanz
+const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
+
+//Läuft einmal wenn client bereit ist
+client.once('ready', () => {
+    console.log('Client ist readyyy!');
+});
+
+//Login to Discord mit Token
+client.login(token);
