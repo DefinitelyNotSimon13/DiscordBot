0ca83f4554737093aebd6178fa4c799292d3a1e5
DefenetlyNotSimon <simon21.blum@gmail.com>
2022-02-24 21:04:05 +0100

    the same as before

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.once('ready', () => {
 
 //Client tut etwas onItneraction
 client.on('interactionCreate', async interaction => {
-    console.log(`${interaction.user.tag} in #${interaction.channel.name} hat eine Interaktion getriggert.`)
+    console.log(`${interaction.user.tag} in #${interaction.channel.name}: Hat eine Interaktion getriggert.`)
 	if (!interaction.isCommand()) return;
 
     const command  = client.commands.get(interaction.commandName);
