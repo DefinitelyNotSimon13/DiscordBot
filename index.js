a7bbd47e8a8fd0326447546621e3e842a6ec16b9
DefenetlyNotSimon <simon21.blum@gmail.com>
2022-02-24 20:44:37 +0100

    On the way to command handler

--- index.js
+++ index.js
@@ -10,5 +10,29 @@ client.once('ready', () => {
     console.log('Client ist readyyy!');
 });
 
+//Client tut etwas onItneraction
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+    
+    
+    //Alter Dingens
+    /*if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server Name: ${interaction.guild.name}\nMitgliederzahl: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Dein Tag: ${interaction.user.tag}\Deine Id: ${interaction.user.id}`);
+	}*/
+});
+
 //Login to Discord mit Token
 client.login(token);
